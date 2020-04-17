cd60f956d877a88d1cb177de0993a1a1bf28e203
CJ <jarac.carlos@gmail.com>
2020-04-16 23:47:14 -0400

    Commit de prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel test1
+                    Laravel test
                 </div>
 
                 <div class="links">
